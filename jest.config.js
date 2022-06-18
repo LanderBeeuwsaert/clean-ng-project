478e2ac0b0eefa0db0f8377338b203ac171220fb
Lander Beeuwsaert <lander.beeuwsaert@gmail.com>
2022-06-18 18:29:31 +0200

    follow instructions on jest-preset-angular

--- jest.config.js
+++ jest.config.js
@@ -1,41 +1,63 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig');
+const { pathsToModuleNameMapper } = require('ts-jest/dist/config');
+const { paths } = require('./tsconfig.json').compilerOptions;
 
-// console.log(compilerOptions);
-// 'moduleNameMapper': pathsToModuleNameMapper(compilerOptions.paths || {}, {
-//     prefix: '<rootDir>/'
-// });
-
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'jest-preset-angular',
-  moduleDirectories: ['node_modules', 'src'],
-  roots: ['<rootDir>/spec/tests'],
-  testMatch: ['**/+(*.)+(spec).+(ts)'],
-  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
-  collectCoverage: true,
-  coverageReporters: ['html'],
-  coverageDirectory: 'coverage/my-app',
-  testEnvironment: 'jsdom', //default
-
+  preset: 'jest-preset-angular/presets/defaults-esm',
   globals: {
     'ts-jest': {
-      allowSyntheticDefaultImports: true
-    }
-  },
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  },
-  'moduleNameMapper': pathsToModuleNameMapper(
-    {
-      ...compilerOptions.paths,
-      // necessary for jest to work for unit testing functions
-      // https://github.com/firebase/firebase-admin-node/issues/1488#issuecomment-1008290036
-      'firebase-admin/*': ['../functions/node_modules/firebase-admin/lib/*']
+      useESM: true,
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      tsconfig: '<rootDir>/tsconfig-esm.spec.json',
     },
-    {
-    prefix: '<rootDir>/src'
-  })
+  },
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
+    tslib: 'tslib/tslib.es6.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/spec/config/setup-jest.ts'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
+
+// const { pathsToModuleNameMapper } = require('ts-jest');
+// const { compilerOptions } = require('./tsconfig');
+//
+// // console.log(compilerOptions);
+// // 'moduleNameMapper': pathsToModuleNameMapper(compilerOptions.paths || {}, {
+// //     prefix: '<rootDir>/'
+// // });
+//
+// module.exports = {
+//   preset: 'jest-preset-angular',
+//   moduleDirectories: ['node_modules', 'src'],
+//   roots: ['<rootDir>/spec/tests'],
+//   testMatch: ['**/+(*.)+(spec).+(ts)'],
+//   setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+//   collectCoverage: true,
+//   coverageReporters: ['html'],
+//   coverageDirectory: 'coverage/my-app',
+//   testEnvironment: 'jsdom', //default
+//
+//   globals: {
+//     'ts-jest': {
+//       allowSyntheticDefaultImports: true
+//     }
+//   },
+//   transform: {
+//     '^.+\\.js$': 'babel-jest'
+//   },
+//   'moduleNameMapper': pathsToModuleNameMapper(
+//     {
+//       ...compilerOptions.paths,
+//       // necessary for jest to work for unit testing functions
+//       // https://github.com/firebase/firebase-admin-node/issues/1488#issuecomment-1008290036
+//       'firebase-admin/*': ['../functions/node_modules/firebase-admin/lib/*']
+//     },
+//     {
+//     prefix: '<rootDir>/src'
+//   })
+// };
 
 // console.log(module.exports);
 
